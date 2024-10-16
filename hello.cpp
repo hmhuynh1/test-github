841e70140b3c3b04dbccdd60518ea8736a9598a7
hmhuynh1 <hmhuynh1@dmacc.edu>
2024-10-15 22:38:43 -0500

    test

---
+++ hello.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int square(int x) {
+	return (x*x);
+}
+
+int cube(int x) {
+	return (x*x)*x;
+}
+
+int main(void) {
+	int s, c;
+    cout << "Enter a number to be squared: ";
+	cin >> s;
+	cout << "The square of " << s << " is " << square(s) << endl;
+	cout << "Enter a number to be cubed: ";
+	cin >> c;
+	cout << "The cube of " << c << " is " << cube(c) << endl;
+    return 0;
+}
+
